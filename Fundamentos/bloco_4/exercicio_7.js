28befca83b728beb0de19c097a4da0a3bc7e5044
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-06-23 16:19:33 -0300

    Exercicio 7

---
+++ Fundamentos/bloco_4/exercicio_7.js
@@ -0,0 +1,29 @@
+let nota = 49;
+
+if (nota>=90 && nota <=100) {
+  nota = "A";
+  console.log(nota);
+}
+else if (nota>=80) {
+  nota = "B";
+  console.log(nota);
+}
+else if (nota>=70) {
+  nota = "C";
+  console.log(nota);
+}
+else if (nota>=60) {
+  nota = "D";
+  console.log(nota);
+}
+else if (nota>=50) {
+  nota = "E";
+  console.log(nota);
+}
+else if (nota >=0) {
+  nota = "F";
+  console.log(nota);
+}
+else {
+  console.log("Nota errada");
+}
