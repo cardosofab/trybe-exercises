c51eb41ddd164b6220d90aa2ca9f9f9c1b0f61d5
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-06-25 15:13:41 -0300

    Exercicio 1

---
+++ Fundamentos/bloco_4/dia_3/exercicio_1.js
@@ -0,0 +1,12 @@
+let n = 5;
+let asterisco = '*';
+let espacoBranco = '';
+
+for (let i = 0; i<n; i+=1) {
+  espacoBranco = espacoBranco + asterisco;
+}
+for (let j=0; j<n; j+=1) {
+  console.log(espacoBranco);
+}
+
+
