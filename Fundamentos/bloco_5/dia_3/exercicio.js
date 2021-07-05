1284e9058b5506f1a156c89009672cf8dacf1687
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-07-05 08:02:07 -0300

    Criando a funcao que adiciona o botao Feriados

--- Fundamentos/bloco_5/dia_3/exercicio.js
+++ Fundamentos/bloco_5/dia_3/exercicio.js
@@ -47,3 +47,21 @@ function preencherDias() {
 }
 preencherDias();
 
+// Exercício 2:
+
+// Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+
+// Adicione a este botão a ID "btn-holiday" .
+
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+
+function botao(string) {
+  let divBotao = document.querySelector('.buttons-container');
+  let botaoFeriados = document.createElement('button');
+  botaoFeriados.innerText = string;
+  botaoFeriados.id = 'btn-holiday';
+  divBotao.appendChild(botaoFeriados);
+}
+
+botao('Feriados');
+
