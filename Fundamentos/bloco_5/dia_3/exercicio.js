ef28963e5d0715ef64420fe15266c55626ae54c5
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-07-05 23:05:39 -0300

    Criando legenda para as tarefas

--- Fundamentos/bloco_5/dia_3/exercicio.js
+++ Fundamentos/bloco_5/dia_3/exercicio.js
@@ -162,3 +162,22 @@ function tarefaPersonalizada(tarefa) {
  }
 
  tarefaPersonalizada('Cozinhar');
+
+//  Exercício 8:
+
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+function labelColor(cor) {
+  let divTarefa = document.querySelector('.my-tasks');
+  let divLegenda = document.createElement('div');
+  divLegenda.className = 'task';
+  divLegenda.style.backgroundColor = cor;
+  divTarefa.appendChild(divLegenda);
+}
+
+labelColor('#f1f1f1');
+
