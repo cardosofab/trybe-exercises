d7b973a7c93c8dbe3b24c536b21e292b907a1777
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-07-22 18:20:48 -0300

    Exercicio 1 - Parte II

--- Fundamentos/bloco_7/dia_1/script.js
+++ Fundamentos/bloco_7/dia_1/script.js
@@ -53,3 +53,21 @@ const oddsAndEvens = array => {
 }
 
 oddsAndEvens([13,3,4,10,7,2]);
+
+// Parte II
+
+// Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let , const , arrow functions , template literals e ternary operator .
+
+// Crie uma função que receba um número e retorne seu fatorial.
+
+// Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+
+const fatorial = number => {
+  let resultado = 1;
+  for (let index = 1; index <= number; index += 1) {
+    resultado *= index;
+  }
+  return resultado;
+}
+
+console.log(fatorial(0));
