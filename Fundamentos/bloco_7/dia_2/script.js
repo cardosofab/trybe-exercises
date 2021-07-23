1e9cce64060e5bc403fa1c5d309bf7c55f2a9eeb
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-07-23 02:19:06 -0300

    Exercicio - Parte III

--- Fundamentos/bloco_7/dia_2/script.js
+++ Fundamentos/bloco_7/dia_2/script.js
@@ -60,3 +60,28 @@ const listStudent = () => {
   console.log('Estudante 2');
   listSkills(student2);
 
+  const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+  const listSkillsValuesWithFor = (student) => {
+    const skills = [];
+    for(skill in student) {
+      skills.push(student[skill]);
+    }
+  
+    return skills;
+  };
+  
+  const listSkillsValuesWithValues = (student) => Object.values(student);
+  
+  // Sem Object.values
+  console.log(listSkillsValuesWithFor(student));
+  
+  // Com Object.values
+  console.log(listSkillsValuesWithValues(student));
+
+
