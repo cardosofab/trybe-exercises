0f6fc4bd1e9b0fece1256a956bfaf088df7f083e
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-07-24 06:31:30 -0300

    Exercicio 6 Pratica - Parte II

--- Fundamentos/bloco_7/dia_2/parteII.js
+++ Fundamentos/bloco_7/dia_2/parteII.js
@@ -90,4 +90,14 @@ const numberStudents = () => {
 
 numberStudents();
 
+// 7.Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
 
+
+const getValueByNumber = (object, positionKey) => {
+  const objectValues = Object.values(object);
+  const value = objectValues[positionKey];
+  return value;
+}
+
+console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matématica'
