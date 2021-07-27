a1aa761512f783d693564e40fdefa02633882b07
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-07-27 06:03:07 -0300

    Exercicio 1 TDD + enunciado - Parte III

--- Fundamentos/bloco_7/dia_3/teste-greetPeople.js
+++ Fundamentos/bloco_7/dia_3/teste-greetPeople.js
@@ -1,3 +1,5 @@
+// Use a variável parameter como parâmetro da função abaixo, escreva testes para verificar se a mesma está retornando como se vê na variável result e, caso não esteja, altere o código para que ele passe nos testes.
+
 const assert = require('assert');
 
 const greetPeople = (people) => {
