1959a18ea61cfa137e8f77874d12a9340b426dca
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-08-03 10:26:12 -0300

    Exercicio 3 - Pratica

--- Fundamentos/bloco_8/dia_3/exercicio.js
+++ Fundamentos/bloco_8/dia_3/exercicio.js
@@ -117,3 +117,45 @@ function nameAndAge() {
   return books.map((book) => ({ age: book.releaseYear - book.author.birthYear, author: book.author.name} )).sort((a, b) => a.age - b.age);
 }
 assert.deepStrictEqual(nameAndAge(), expectedResult1);
+
+// 3 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+// Dica: use as função filter ;
+
+const expectedResult2 = [
+  { 
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948 },
+    releaseYear: 1991
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+    releaseYear: 1954
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920 },
+    releaseYear: 1951
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920 },
+    releaseYear: 1965
+  }
+];
+
+function fantasyOrScienceFiction() {
+  // escreva seu código aqui
+  return books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+}
+
+assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult2);
+
