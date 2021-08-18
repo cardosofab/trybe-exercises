b5079d6d51fa38620366f107206af8186d0e706d
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-08-18 10:48:24 -0300

    Implementando testes da funçao sum

--- Fundamentos/bloco_10/dia_1/my_new_project/sum.test.js
+++ Fundamentos/bloco_10/dia_1/my_new_project/sum.test.js
@@ -1,6 +1,25 @@
 // sum.test.js
 const sum = require('./sum');
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toBe(5);
+describe('Testes da função sum', () => {
+  it('sums two values', () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+  it('Soma 4 + 5', () => {
+    expect(sum(5, 4)).toBe(9);
+  });
+  it('Soma 0 + 0', () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+  it('throws an error when a string is passed', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow();
+  });
+  test('error message is "parameters must be numbers"', () => {
+    expect(() => {
+      sum(4, '5');
+    }).toThrow(/parameters must be numbers/);
+  });
 });
+
