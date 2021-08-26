648ef96a065fbac550b05dd5679474b2a65b57cf
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-08-26 14:40:55 -0300

    Exercicio 3

--- Fundamentos/bloco_10/dia_3/test/randomNumber.spec.js
+++ Fundamentos/bloco_10/dia_3/test/randomNumber.spec.js
@@ -18,3 +18,28 @@ test("testando se a função foi chamada, qual seu retorno, quantas vezes foi ch
   expect(service.randomNumber).toHaveBeenCalledTimes(1);
   expect(service.randomNumber).toHaveBeenCalledWith(10, 2);
 });
+
+// 3 - Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
+
+describe("testando implementações", () => {
+  test("mockando função para receber 3 parâmetros e retornar sua multiplicação", () => {
+    service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+    expect(service.randomNumber(2, 3, 4)).toBe(24);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(2, 3, 4);
+  });
+
+  test("mockando função que recebe um parâmetro e retorna seu dobro", () => {
+    service.randomNumber.mockReset();
+    expect(service.randomNumber).toHaveBeenCalledTimes(0);
+
+    service.randomNumber.mockImplementation(a => a * 2);
+
+    expect(service.randomNumber(5)).toBe(10);
+    expect(service.randomNumber).toHaveBeenCalled();
+    expect(service.randomNumber).toHaveBeenCalledTimes(1);
+    expect(service.randomNumber).toHaveBeenCalledWith(5);
+  });
+});
