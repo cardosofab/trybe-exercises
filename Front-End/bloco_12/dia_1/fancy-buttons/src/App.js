1c52c7a5edddbe96496f67d0e7612cdce9d506c1
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-09-02 01:49:26 -0300

    Alterando a cor do botao caso a quantidade de clique seja par

--- Front-End/bloco_12/dia_1/fancy-buttons/src/App.js
+++ Front-End/bloco_12/dia_1/fancy-buttons/src/App.js
@@ -15,31 +15,54 @@ class App extends Component {
     this.twoClick = this.twoClick.bind(this)
     this.threeClick = this.threeClick.bind(this)
   }
+
+  changeColor(state) {
+    return state % 2 === 0 ? 'green' : 'blue';
+  }
   
   oneClick() {
     this.setState((estadoAnterior, _props) => ({
       oneCliques: estadoAnterior.oneCliques + 1
-    }))
+    }), () => {
+      console.log(`Botão 1 ${this.changeColor(this.state.oneCliques)}`);
+    });
   }
   
   twoClick() {
     this.setState((estadoAnterior, _props) => ({
       twoCliques: estadoAnterior.twoCliques + 1
-    }))
+    }), () => {
+      console.log(`Botão 2 ${this.changeColor(this.state.twoCliques)}`);
+    });
   }
   
   threeClick() {
     this.setState((estadoAnterior, _props) => ({
       threeCliques: estadoAnterior.threeCliques + 1
-    }))
+    }), () => {
+      console.log(`Botão 3 ${this.changeColor(this.state.threeCliques)}`);
+    });
   }
 
   render() {
+    const { oneCliques, twoCliques, threeCliques } = this.state;
     return (
       <div>
-        <button onClick={this.oneClick}>{this.state.oneCliques}</button>
-        <button onClick={this.twoClick}>{this.state.twoCliques}</button>
-        <button onClick={this.threeClick}>{this.state.threeCliques}</button>
+        <button 
+          onClick={this.oneClick} 
+          style={{backgroundColor: this.changeColor(oneCliques)}}>
+            {this.state.oneCliques}
+        </button>
+        <button 
+          onClick={this.twoClick} 
+          style={{backgroundColor: this.changeColor(twoCliques)}}>
+            {this.state.twoCliques}
+        </button>
+        <button 
+          onClick={this.threeClick} 
+          style={{backgroundColor: this.changeColor(threeCliques)}}>
+            {this.state.threeCliques}
+        </button>
       </div>
     );
   }
