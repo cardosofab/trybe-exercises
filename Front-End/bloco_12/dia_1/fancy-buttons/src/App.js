7fa5dbf941a8a0a89dc449ae6ca5b695ac1d0a5c
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-09-01 23:17:33 -0300

    Para fixar - Vinculando funções à classe com this e bind no constructor

--- Front-End/bloco_12/dia_1/fancy-buttons/src/App.js
+++ Front-End/bloco_12/dia_1/fancy-buttons/src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function oneclick() {
-  console.log('Primeiro botão');
-}
-
-function twoclick() {
-  console.log('Segundo botão');
-}
-
-function threeclick() {
-  console.log('Terceiro botão');
-}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.oneClick = this.oneClick.bind(this)
+    this.twoClick = this.twoClick.bind(this)
+    this.threeClick = this.threeClick.bind(this)
+  }
+  
+  oneClick() {
+    console.log('"this" do botão 1 :', this);
+  }
+  
+  twoClick() {
+    console.log('"this" do botão 2 :', this);
+  }
+  
+  threeClick() {
+    console.log('"this" do botão 3 :', this);
+  }
+
   render() {
     return (
       <div>
-        <button onClick={oneclick}>Primeiro botão</button>
-        <button onClick={twoclick}>Segundo botão</button>
-        <button onClick={threeclick}>Terceiro botão</button>
+        <button onClick={this.oneClick}>Primeiro botão</button>
+        <button onClick={this.twoClick}>Segundo botão</button>
+        <button onClick={this.threeClick}>Terceiro botão</button>
       </div>
     );
   }
