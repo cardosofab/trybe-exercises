d54f5edc7ace3740cb6e4e8872c3b541876906df
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-09-01 22:42:26 -0300

    Para fixar - Introduçao aos eventos

--- Front-End/bloco_12/dia_1/fancy-buttons/src/App.js
+++ Front-End/bloco_12/dia_1/fancy-buttons/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function oneclick() {
+  console.log('Primeiro botão');
+}
+
+function twoclick() {
+  console.log('Segundo botão');
+}
+
+function threeclick() {
+  console.log('Terceiro botão');
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={oneclick}>Primeiro botão</button>
+        <button onClick={twoclick}>Segundo botão</button>
+        <button onClick={threeclick}>Terceiro botão</button>
+      </div>
+    );
+  }
 }
 
 export default App;
