bc8dc2ad4adbf2e24716ba0fa51e699530c14b7d
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-10-14 09:33:06 -0300

    Retirando o Provider do arquivo App por conta dos testes

--- Front-End/bloco_16/dia_5/counter/src/App.js
+++ Front-End/bloco_16/dia_5/counter/src/App.js
@@ -1,17 +1,17 @@
 // src/App.js
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
+// import { Provider } from 'react-redux';
+// import store from './store';
 import ButtonClicks from './ButtonClicks';
 import NumberClicks from './NumberClicks';
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Provider store={store}>
+        {/* <Provider store={store}> */}
           <ButtonClicks />
           <NumberClicks />
-        </Provider>
+        {/* </Provider> */}
       </div>
     );
   }
