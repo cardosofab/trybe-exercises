d99bcde5c9628d4b4fb4d1506772aef244466912
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2022-01-16 01:27:37 -0300

    Exercicio 4.2 concluido

---
+++ Back-End/bloco_22/dia_2/exercise4.2.js
@@ -0,0 +1,18 @@
+const fs = require('fs').promises;
+
+async function getSimpsonById(id) {
+  const simpsons = await fs
+    .readFile('./simpsons.json', 'utf-8')
+    .then((fileContent) => JSON.parse(fileContent));
+
+  const chosenSimpson = simpsons.find((simpson) => simpson.id === id);
+
+  if (!chosenSimpson) {
+    throw new Error('id não encontrado');
+  }
+  return chosenSimpson;
+}
+
+getSimpsonById('5')
+  .then(response => console.log(response));
+
