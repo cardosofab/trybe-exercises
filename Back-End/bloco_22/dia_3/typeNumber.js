27d500a8747336e9db991e5b9e258443193c6c28
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2022-01-17 00:22:00 -0300

    Finaliza funçao que verifica numero

--- Back-End/bloco_22/dia_3/typeNumber.js
+++ Back-End/bloco_22/dia_3/typeNumber.js
@@ -1,4 +1,7 @@
-function typeNumber(number) {
+const readline = require('readline-sync');
+
+function typeNumber() {
+  const number = readline.questionInt('Digite um número: ');
   if (number > 0) {
     return 'Positivo';
   } else if (number < 0) {
@@ -7,3 +10,7 @@ function typeNumber(number) {
     return 'Neutro';
   }
 };
+
+module.exports = {
+  typeNumber,
+}
