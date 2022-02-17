b7bd4d463362ba44d4f6b58400967a072f4cac8f
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2022-02-16 21:35:13 -0300

    Exercise 1 - Practice

---
+++ Back-End/bloco_26/dia_1/my-ts-scripts/capacity.ts
@@ -0,0 +1,8 @@
+import utils from "./utils";
+
+const units = ["kl", "hl", "dal", "l", "dl", "cl", "ml"];
+
+function convert(value: number, forUnity: string, toUnity: string): number {
+
+    return utils.convert(units, value, forUnity, toUnity);
+}
---
+++ Back-End/bloco_26/dia_1/my-ts-scripts/length.ts
@@ -0,0 +1,8 @@
+import utils from "./utils";
+
+const units = ["km", "hm", "dam", "m", "dm", "cm", "mm"];
+
+function convert(value: number, forUnity: string, toUnity: string): number {
+
+    return utils.convert(units, value, forUnity, toUnity);
+}
