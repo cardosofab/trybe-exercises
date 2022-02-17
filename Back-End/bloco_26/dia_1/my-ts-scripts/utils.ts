47a81aef40844bebe8004b2630907aaee0202926
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2022-02-17 01:16:27 -0300

    Exercise 7 - Concluded

--- Back-End/bloco_26/dia_1/my-ts-scripts/utils.ts
+++ Back-End/bloco_26/dia_1/my-ts-scripts/utils.ts
@@ -1,21 +1,42 @@
+import readline from "readline-sync";
+
 function makeError(unity: string) {
-  throw new Error(`A unidade ${unity} não é uma unidade válida.`)
+    throw new Error(`A unidade ${unity} não é uma unidade válida.`)
 }
 
-// nossa função convert gora recebe como primeiro parâmetro um array de strings com as unidades de medida
 function convert(units: string[], value: number, forUnity: string, toUnity: string): number {
 
-  if (!units.includes(forUnity)) makeError(forUnity); // se a unidade base não for válida lançamos um erro
-  if (!units.includes(toUnity)) makeError(toUnity); // se a unidade para a conversão não for válida lançamos um erro
+    if (!units.includes(forUnity)) makeError(forUnity);
+    if (!units.includes(toUnity)) makeError(toUnity);
 
-  const forIndex = units.indexOf(forUnity); // pegamos o index da unidade base no array
-  const toIndex = units.indexOf(toUnity); // pegamos o index da unidade para a conversão
-  const exponent = (toIndex - forIndex); // calculamos o expoente a partir da diferença dos index
+    const forIndex = units.indexOf(forUnity);
+    const toIndex = units.indexOf(toUnity);
+    const exponent = (toIndex - forIndex);
 
-  return value * Math.pow(10, exponent);
+    return value * Math.pow(10, exponent);
 }
 
-// exportamos nossa função para que os demais scripts possam utilizar
+function exec(units: string[]) {
+    const value = readline.questionFloat("Digite o valor a ser convertido: \n");
+
+    const forUnityChoice = readline.keyInSelect(units, "Escolha um número para a unidade base:", { cancel: "SAIR" });
+
+    if (forUnityChoice === -1) return console.log("Saindo!");
+
+    const toUnityChoice = readline.keyInSelect(units, "Escolha um número para a conversão:", { cancel: "SAIR" });
+
+    if (toUnityChoice === -1) return console.log("Saindo!");
+
+    const forUnity = units[forUnityChoice];
+    const toUnity = units[toUnityChoice];
+    const result = convert(units, value, forUnity, toUnity);
+
+    const message = `${value}${units[forUnityChoice]} é igual a ${result}${units[toUnityChoice]}`
+
+    console.log(message);
+}
+
 export default {
-  convert
+    convert,
+    exec
 }
